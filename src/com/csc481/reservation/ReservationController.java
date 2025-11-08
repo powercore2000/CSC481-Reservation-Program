f7ff61d0c1a6a24b544d38351a8ca72568e1955e
powercore2000 <bcory00@yahoo.com>
2025-11-07 23:32:11 -0800

    Installed spring boost and maven
    You need to open the project as an existing maven project to run properly. And to install maven on your computer

--- src/com/csc481/reservation/ReservationController.java
+++ src/com/csc481/reservation/ReservationController.java
@@ -1,5 +1,6 @@
+package com.csc481.reservation;
 
-public class Main {
+public class ReservationController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/main/java/com/csc481/reservationSys/ReservationApplication.java
@@ -0,0 +1,14 @@
+package com.csc481.reservationSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationApplication.class, args);
+	}
+
+}
