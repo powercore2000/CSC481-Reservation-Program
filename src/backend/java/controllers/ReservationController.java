1ccb62a4e9501e44d256daf85839326fc6f67311
powercore2000 <bcory00@yahoo.com>
2025-11-15 20:41:23 -0800

    Update ReservationController.java

--- src/backend/java/controllers/ReservationController.java
+++ src/backend/java/controllers/ReservationController.java
@@ -1,14 +1,11 @@
-package backend.java;
+package backend.java.controllers;
 
 import backend.java.models.*;
 
 import java.util.ArrayList;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 
