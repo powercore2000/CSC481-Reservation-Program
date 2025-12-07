60d39f1e1842b36613454a86abb23210f4abfd71
Daniel Anzora <anzora5292@gmail.com>
2025-12-07 08:29:43 -0800

    Aligns to SQLite

--- src/main/java/database/UserQueries.java
+++ src/main/java/database/UserQueries.java
@@ -153,7 +153,8 @@ public class UserQueries
 
     /* ----------------- SAVE (INSERT or UPDATE) ----------------- */
 
-    /* Saves a user using email as the unique identifier
+    /**
+     * Saves a user using email as the unique identifier
     * If no user with that email exist --> INSERT
     * If user exist --> UPDATE
     * */
