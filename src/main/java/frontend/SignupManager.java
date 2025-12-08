de7cc1d184641a1a7a270c0a836b19492338cb39
powercore2000 <bcory00@yahoo.com>
2025-12-08 02:37:21 -0800

    Sign Up

--- src/main/java/frontend/SignupManager.java
+++ src/main/java/frontend/SignupManager.java
@@ -6,15 +6,19 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class SignupController
+import database.dto.UserDTO;
+
+public class SignupManager
 {
 
-    @FXML private TextField usernameField;
+    @FXML private TextField fullNameField;
     @FXML private TextField emailField;
+    @FXML private TextField phoneField;
     @FXML private PasswordField passwordField;
     @FXML private PasswordField confirmPasswordField;
     @FXML private Label errorLabel;
@@ -22,16 +26,22 @@ public class SignupController
     private Stage getStage(ActionEvent event) {
         return (Stage) ((Node) event.getSource()).getScene().getWindow();
     }
+    
+    public void initalize() {
+    	phoneField.setTextFormatter(new TextFormatter<>(change -> {
+    	    return change.getControlNewText().matches("[0-9()\\-\\s]*") ? change : null;
+    	}));
+    }
 
     @FXML
     private void onSignupClick(ActionEvent event) throws IOException {
-        String username = usernameField.getText().trim();
+        String fullName = fullNameField.getText().trim();
         String email = emailField.getText().trim();
         String pw = passwordField.getText();
         String pw2 = confirmPasswordField.getText();
 
         // Very simple validation (no real database here)
-        if (username.isEmpty() || email.isEmpty() || pw.isEmpty() || pw2.isEmpty()) {
+        if (fullName.isEmpty() || email.isEmpty() || pw.isEmpty() || pw2.isEmpty()) {
             errorLabel.setText("Please fill in all fields.");
             return;
         }
@@ -41,6 +51,16 @@ public class SignupController
             return;
         }
 
+        UserDTO signUpUser = new UserDTO(fullNameField,email,password);
+        Boolean signInState = backend.controllers.UserController.loginUser(loginUser);
+        
+        if(!signInState) {
+        	errorLabel.setText("No user found with that username and password.");
+        	return;
+        }
+        // this helps pretend login success
+        AppState.setSignedIn(true);
+        
         // “Create” account – in this project we just mark the user as signed in
         AppState.setSignedIn(true);
         // optional: remember username if you want to show it later
